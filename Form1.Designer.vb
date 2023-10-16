f6feaec6b346b60e16d088b0354dec4040bae650
Khair Uddin <ku617754@gmail.com>
2023-10-16 11:36:07 +0700

    menambahkan fitur

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,20 +24,29 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Label1 = New Label()
         GroupBox1 = New GroupBox()
-        GroupBox2 = New GroupBox()
-        Button1 = New Button()
-        Label2 = New Label()
+        ComboBox2 = New ComboBox()
+        Label9 = New Label()
         TextBox1 = New TextBox()
-        Label3 = New Label()
-        Label4 = New Label()
-        Label5 = New Label()
-        RadioButton1 = New RadioButton()
-        RadioButton2 = New RadioButton()
-        ComboBox1 = New ComboBox()
+        Label2 = New Label()
+        GroupBox2 = New GroupBox()
         TextBox2 = New TextBox()
+        ComboBox1 = New ComboBox()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
         Label6 = New Label()
         Label7 = New Label()
         Label8 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Button2 = New Button()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
         GroupBox1.SuspendLayout()
         GroupBox2.SuspendLayout()
         SuspendLayout()
@@ -45,22 +54,60 @@ Partial Class Form1
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(321, 35)
+        Label1.Font = New Font("Stencil", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(268, 21)
         Label1.Name = "Label1"
-        Label1.Size = New Size(95, 15)
+        Label1.Size = New Size(228, 32)
         Label1.TabIndex = 0
         Label1.Text = "PROGRAM SPBU"
         ' 
         ' GroupBox1
         ' 
+        GroupBox1.Controls.Add(ComboBox2)
+        GroupBox1.Controls.Add(Label9)
         GroupBox1.Controls.Add(TextBox1)
         GroupBox1.Controls.Add(Label2)
-        GroupBox1.Location = New Point(240, 74)
+        GroupBox1.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        GroupBox1.Location = New Point(193, 75)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(246, 76)
+        GroupBox1.Size = New Size(387, 118)
         GroupBox1.TabIndex = 1
         GroupBox1.TabStop = False
         GroupBox1.Text = "[ INPUT HARGA ]"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(183, 31)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(193, 26)
+        ComboBox2.TabIndex = 3
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(6, 31)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(104, 18)
+        Label9.TabIndex = 2
+        Label9.Text = "JENIS BBM"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Enabled = False
+        TextBox1.Location = New Point(183, 75)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(193, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(6, 78)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(171, 18)
+        Label2.TabIndex = 0
+        Label2.Text = "HARGA PER LITER"
         ' 
         ' GroupBox2
         ' 
@@ -71,136 +118,194 @@ Partial Class Form1
         GroupBox2.Controls.Add(Label5)
         GroupBox2.Controls.Add(Label4)
         GroupBox2.Controls.Add(Label3)
-        GroupBox2.Location = New Point(41, 203)
+        GroupBox2.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        GroupBox2.Location = New Point(34, 237)
         GroupBox2.Name = "GroupBox2"
-        GroupBox2.Size = New Size(286, 186)
+        GroupBox2.Size = New Size(380, 186)
         GroupBox2.TabIndex = 2
         GroupBox2.TabStop = False
         GroupBox2.Text = "BELI BERDASARKAN"
         ' 
-        ' Button1
+        ' TextBox2
         ' 
-        Button1.Location = New Point(490, 203)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(100, 25)
-        Button1.TabIndex = 3
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
+        TextBox2.Location = New Point(213, 125)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(130, 27)
+        TextBox2.TabIndex = 4
         ' 
-        ' Label2
+        ' ComboBox1
         ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(6, 34)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(101, 15)
-        Label2.TabIndex = 0
-        Label2.Text = "HARGA PER LITER"
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(213, 29)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(130, 26)
+        ComboBox1.TabIndex = 4
         ' 
-        ' TextBox1
+        ' RadioButton2
         ' 
-        TextBox1.Location = New Point(122, 31)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(100, 23)
-        TextBox1.TabIndex = 1
-        TextBox1.Text = "10000"
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(213, 94)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(77, 22)
+        RadioButton2.TabIndex = 4
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "UANG"
+        RadioButton2.UseVisualStyleBackColor = True
         ' 
-        ' Label3
+        ' RadioButton1
         ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(6, 29)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(108, 15)
-        Label3.TabIndex = 4
-        Label3.Text = "JENIS KENDARAAN"
-        ' 
-        ' Label4
-        ' 
-        Label4.AutoSize = True
-        Label4.Location = New Point(6, 63)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(113, 15)
-        Label4.TabIndex = 4
-        Label4.Text = "BELI BERDASARKAN"
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(213, 63)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(80, 22)
+        RadioButton1.TabIndex = 4
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "LITER"
+        RadioButton1.UseVisualStyleBackColor = True
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
         Label5.Location = New Point(6, 128)
         Label5.Name = "Label5"
-        Label5.Size = New Size(122, 15)
+        Label5.Size = New Size(201, 18)
         Label5.TabIndex = 4
         Label5.Text = "JUMLAH UANG/LITER"
         ' 
-        ' RadioButton1
+        ' Label4
         ' 
-        RadioButton1.AutoSize = True
-        RadioButton1.Location = New Point(138, 63)
-        RadioButton1.Name = "RadioButton1"
-        RadioButton1.Size = New Size(53, 19)
-        RadioButton1.TabIndex = 4
-        RadioButton1.TabStop = True
-        RadioButton1.Text = "LITER"
-        RadioButton1.UseVisualStyleBackColor = True
+        Label4.AutoSize = True
+        Label4.Location = New Point(6, 63)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(188, 18)
+        Label4.TabIndex = 4
+        Label4.Text = "BELI BERDASARKAN"
         ' 
-        ' RadioButton2
+        ' Label3
         ' 
-        RadioButton2.AutoSize = True
-        RadioButton2.Location = New Point(138, 88)
-        RadioButton2.Name = "RadioButton2"
-        RadioButton2.Size = New Size(58, 19)
-        RadioButton2.TabIndex = 4
-        RadioButton2.TabStop = True
-        RadioButton2.Text = "UANG"
-        RadioButton2.UseVisualStyleBackColor = True
+        Label3.AutoSize = True
+        Label3.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(6, 29)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(176, 18)
+        Label3.TabIndex = 4
+        Label3.Text = "JENIS KENDARAAN"
         ' 
-        ' ComboBox1
+        ' Button1
         ' 
-        ComboBox1.FormattingEnabled = True
-        ComboBox1.Location = New Point(138, 26)
-        ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(121, 23)
-        ComboBox1.TabIndex = 4
-        ' 
-        ' TextBox2
-        ' 
-        TextBox2.Location = New Point(138, 125)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(100, 23)
-        TextBox2.TabIndex = 4
+        Button1.Location = New Point(668, 227)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(100, 25)
+        Button1.TabIndex = 3
+        Button1.Text = "Proses"
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
-        Label6.Location = New Point(388, 237)
+        Label6.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(447, 278)
         Label6.Name = "Label6"
-        Label6.Size = New Size(124, 15)
+        Label6.Size = New Size(197, 18)
         Label6.TabIndex = 4
         Label6.Text = "KENDARAAN RODA   :"
         ' 
         ' Label7
         ' 
         Label7.AutoSize = True
-        Label7.Location = New Point(388, 282)
+        Label7.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(447, 327)
         Label7.Name = "Label7"
-        Label7.Size = New Size(125, 15)
+        Label7.Size = New Size(197, 18)
         Label7.TabIndex = 5
         Label7.Text = "JUMLAH UANG           :"
         ' 
         ' Label8
         ' 
         Label8.AutoSize = True
-        Label8.Location = New Point(388, 331)
+        Label8.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(448, 378)
         Label8.Name = "Label8"
-        Label8.Size = New Size(123, 15)
+        Label8.Size = New Size(195, 18)
         Label8.TabIndex = 6
-        Label8.Text = "JUMLAH LITER            :"
+        Label8.Text = "JUMLAH LITER          :"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(519, 282)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(0, 15)
+        Label10.TabIndex = 7
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(517, 331)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(0, 15)
+        Label11.TabIndex = 8
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(724, 418)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(44, 36)
+        Button2.TabIndex = 9
+        Button2.Text = "clear"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(517, 237)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(0, 15)
+        Label12.TabIndex = 10
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label13.Location = New Point(650, 281)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(16, 18)
+        Label13.TabIndex = 11
+        Label13.Text = "-"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label14.Location = New Point(650, 327)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(16, 18)
+        Label14.TabIndex = 12
+        Label14.Text = "-"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label15.Location = New Point(650, 381)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(16, 18)
+        Label15.TabIndex = 13
+        Label15.Text = "-"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.LightSalmon
-        ClientSize = New Size(757, 450)
+        ClientSize = New Size(882, 466)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Button2)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
         Controls.Add(Label8)
         Controls.Add(Label7)
         Controls.Add(Label6)
@@ -234,4 +339,13 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label12 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -4,7 +4,19 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Sepeda Motor")
+        ComboBox1.Items.Add("Bajai")
+        ComboBox1.Items.Add("Mobil")
+        ComboBox1.Items.Add("Truk")
+        ComboBox1.Items.Add("Bus")
 
+
+        ComboBox2.Items.Add("Pertamax")
+        ComboBox2.Items.Add("Pertalite")
+        ComboBox2.Items.Add("Pertamax Turbo")
+        ComboBox2.Items.Add("Dexlite")
+        ComboBox2.Items.Add("Pertamina Dex")
+        ComboBox2.Items.Add("Pertamina Biosolar")
     End Sub
 
     Private Sub Label4_Click(sender As Object, e As EventArgs)
@@ -13,4 +25,62 @@
 
     Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
     End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Select Case ComboBox2.Text
+            Case "Pertamax"
+                TextBox1.Text = 12400
+            Case "Pertalite"
+                TextBox1.Text = 10000
+            Case "Pertamax Turbo"
+                TextBox1.Text = 14400
+            Case "Dexlite"
+                TextBox1.Text = 13950
+            Case "Pertamina Dex"
+                TextBox1.Text = 14350
+            Case "Pertamina Biosolar"
+                TextBox1.Text = 6800
+        End Select
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Select Case ComboBox1.Text
+            Case "Sepeda Motor"
+                Label13.Text = "Roda Dua"
+            Case "Bajai"
+                Label13.Text = "Roda Tiga"
+            Case "Mobil"
+                Label13.Text = "Roda Empat / Enam"
+            Case "Truk"
+                Label13.Text = "Roda Empat / Enam"
+            Case "Bus"
+                Label13.Text = "Roda Empat / Enam"
+        End Select
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result As Double = Val(TextBox2.Text) / Val(TextBox1.Text)
+
+        If RadioButton1.Checked = True Then
+            Label14.Text = "Rp. " & Val(TextBox2.Text) * Val(TextBox1.Text)
+            Label15.Text = TextBox2.Text & " Liter"
+        End If
+        If RadioButton2.Checked = True Then
+            Label14.Text = "Rp. " & TextBox2.Text
+            Label15.Text = result.ToString("0.0") & " Liter"
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+        TextBox2.Text = ""
+        Label13.Text = ""
+        Label14.Text = ""
+        Label15.Text = ""
+    End Sub
+
+
 End Class
